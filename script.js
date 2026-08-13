c25d5a621ef485cc152d619b824b2001aefa8e6e
OfriAmir <274250321+OfriAmir@users.noreply.github.com>
2026-08-13 20:18:03 +0300

    Add finished game logic with basic UI

--- script.js
+++ script.js
@@ -45,29 +45,32 @@ const game = (function(){
     let players;
     let board = gameboard.getGameboard();
     let lastGameArr
-    function setPlayers (arr){
-        lastGameArr = arr.slice(); // To use in the next game in case the players don't want to change names or marks (X,O)
-        players = [
-        {
-            name: null,
-            mark: null,
-            active: false,
-            winner: false,
-        },
-        {
-            name: null,
-            mark: null,
-            active: false,
-            winner: false,
-        }];
-        players[0].name = arr[0].name;
-        players[1].name = arr[1].name;
-        players[0].mark = arr[0].mark;
-        players[1].mark = arr[1].mark;
-        if (players[0].mark = "x"){
-            [playerX, playerO] = [players[0],players[1]];
-        } else {
-            [playerX, playerO] = [players[1],players[0]];
+    players = [
+    {
+        name: null,
+        mark: "x",
+        active: false,
+        winner: false,
+        winCount: 0,
+    },
+    {
+        name: null,
+        mark: "o",
+        active: false,
+        winner: false,
+        winCount: 0,
+    }];
+    [playerX, playerO] = [players[0],players[1]]
+    function setPlayers (arr, resetWinCount){
+        lastGameArr = arr.slice(); // To use in the next game in case the players don't want to change names. Used in restartGame()
+        for (let i = 0; i < 2; i++){
+            players[i].name = arr[i];
+            players[i].active = false;
+            players[i].winner = false;
+            if (resetWinCount) {
+                players[i].winCount = 0;
+            }
+            displayController.updateScores()
         }
     };
 
@@ -94,8 +97,11 @@ const game = (function(){
                     if (board[position-1].getValue() == mark) ++sum;//it's indexes so substracted by 1.
                     else break;
                     if (sum == 3){
-                        if (mark == "x") players[0].winner = true;
-                        else players[1].winner = true;
+                        if (mark == "x") {
+                            players[0].winner = true;
+                        } else {
+                            players[1].winner = true;
+                        }
                         return "win";
                     }
                 }
@@ -104,13 +110,10 @@ const game = (function(){
         const tie = board.every(item => item.getValue())
         if (tie) return "tie"
     }
-    function getWinner(){
-        for (let player of players){
-            if (player.winner) return player.name;
-        }
-    }
 
-    function startGame(arr = [{name:"Player 1", mark: "x",},{name: "Player 2", mark: "o"}], changePlayers) {
+    let activePlayer
+
+    function startGame(arr = ["Player 1","Player 2"], changePlayers) {
         if (!players) setPlayers(arr)
         else restartGame(arr, changePlayers)
         activePlayer = playerX //playerX starts in tictactoe
@@ -134,7 +137,9 @@ const game = (function(){
                     displayController.displaySentence(`---It's a tie! No one won!---`)
                     return
                 } else if (gameOverMessage == "win"){
-                    displayController.displaySentence(`---${getWinner()} Won!---`)
+                    activePlayer.winCount++
+                    displayController.updateScores()
+                    displayController.displaySentence(`---${activePlayer.name} Won!---`)
                     return
                 }
             }
@@ -148,44 +153,102 @@ const game = (function(){
 
     }
 
-    function restartGame(arr, changePlayers){
-        if (!(changePlayers == "change players")){
-            arr = lastGameArr
-        }
-        setPlayers(arr)
+    function restartGame(arr, resetWinCount){
+        if (!arr) arr = lastGameArr
+        setPlayers(arr, resetWinCount)
         rounds = 0
         activePlayer = playerX
         activePlayer.active = true
         displayController.displaySentence(`It is ${activePlayer.name}'s turn!`)
         gameboard.resetGameboard()
-        console.log("---Restarted---")
+        displayController.updateGameboard()
     }
-    return {startGame, playTurn, restartGame}
+
+    function getScores () {
+        return [playerX.winCount, playerO.winCount]
+    }
+
+    return {startGame, playTurn, restartGame, getScores}
 })()
 
 
 const displayController = (function(){
-    const squares = document.querySelectorAll(".gameboard-square")
-    const gameboardRef = document.querySelector(".gameboard")
+    let refs = (function() {
+        const squares = document.querySelectorAll(".gameboard-square")
+        const gameboard = document.querySelector(".gameboard")
+        const displaySentence = document.querySelector(".display-sentence")
+        const restartBtn = document.querySelector(".restart-btn")
+        const openSettingsBtn = document.querySelector(".settings-btn")
+        const settingsForm = document.querySelector(".settings-form")
+        const playerX = document.querySelector(".playerX") 
+        const playerO = document.querySelector(".playerO")
+        const scores = document.querySelector(".display-scores")
+
+        return {squares ,gameboard, displaySentence, restartBtn, openSettingsBtn, settingsForm, playerX, playerO, scores,}
+    })()
+
+
     function updateGameboard(){
         let gameboardData = gameboard.getGameboard()
-        squares.forEach((item, index) => {
+        refs.squares.forEach((item, index) => {
             item.textContent = gameboardData[index].getValue()     
         })
     }
-    const text = document.querySelector(".display-sentence")
     function displaySentence(sentence){
-        text.textContent = "hello?"
-        text.textContent = sentence
+        refs.displaySentence.textContent = sentence
     }
-    gameboardRef.addEventListener("click", (e) => {
-        squares.forEach((item, index) => {
+
+    function updateScores(){
+        const scores = game.getScores()
+        const playerXScoreRef = refs.scores.querySelector(".playerX-score")
+        const playerOScoreRef = refs.scores.querySelector(".playerO-score")
+        playerXScoreRef.textContent = scores[0]
+        playerOScoreRef.textContent = scores[1]
+    }
+
+    refs.gameboard.addEventListener("click", (e) => {
+        refs.squares.forEach((item, index) => {
             if (e.target.getAttribute("class") == item.getAttribute("class")){
                 game.playTurn(index)
             }
         })
     })
-    return ({updateGameboard, displaySentence})
+
+    let settingsArr = ["player 1", "player 2"]
+
+    refs.restartBtn.addEventListener("click", function() {
+        game.restartGame(settingsArr)
+    })
+
+    refs.openSettingsBtn.addEventListener("click", function() {
+        refs.settingsForm.toggleAttribute("hidden")
+    })
+
+    refs.settingsForm.addEventListener("click", function(e){
+        if (e.target.getAttribute("class") == "submit-btn"){
+            const playerXName = refs.playerX.value
+            const playerOName = refs.playerO.value
+            settingsArr = [`${playerXName}`, `${playerOName}`]
+            refs.playerX.setAttribute("value", settingsArr[0])
+            refs.playerO.setAttribute("value", settingsArr[1])
+            const playerXNameRef = refs.scores.querySelector(".playerX-name")
+            const playerONameRef = refs.scores.querySelector(".playerO-name")
+            playerXNameRef.textContent = settingsArr[0]
+            playerONameRef.textContent = settingsArr[1]
+
+            const resetScores = refs.settingsForm.querySelector(".reset-scores").checked
+            game.restartGame(settingsArr, resetScores)
+
+            refs.settingsForm.reset()
+            refs.settingsForm.toggleAttribute("hidden")
+
+        } else if (e.target.getAttribute("class") == "cancel-btn"){
+            refs.settingsForm.reset()
+            refs.settingsForm.toggleAttribute("hidden")
+        } 
+
+    })
+    return ({updateGameboard, displaySentence, updateScores,})
 })()
 
 game.startGame()
